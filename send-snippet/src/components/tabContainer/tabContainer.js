ff30581d0562d6a498b8a27a99dd472467cf436e
Nick <nicolaslee2000@gmail.com>
2024-03-04 22:40:27 +0100

    feat: layout complete, box shadow transition in SendTab.js

--- send-snippet/src/components/tabContainer/tabContainer.js
+++ send-snippet/src/components/tabContainer/tabContainer.js
@@ -4,16 +4,18 @@ import ReceiveTab from "../tabs/ReceiveTab/ReceiveTab";
 import SendTab from "../tabs/SendTab/SendTab";
 
 export default function TabContainer() {
-  const [activeTab, setActiveTab] = useState("tab1");
+  const [status, setStatus] = useState("idle");
   return (
     <div className="tabContainer">
-      <div className="tabContainer tabContainer-effect-slide-top tabContainer-theme-1">
+      <div className="tabContainer tabContainer-effect-scale">
         <input
           type="radio"
           name="tabContainer"
           checked
           id="tab1"
           className="tab-content-first"
+          readOnly
+          disabled={status !== "idle"}
         />
         <label htmlFor="tab1">
           <i className="icon-bolt"></i>Send
@@ -24,25 +26,22 @@ export default function TabContainer() {
           name="tabContainer"
           id="tab2"
           className="tab-content-2"
+          readOnly
+          disabled={status !== "idle"}
         />
         <label htmlFor="tab2">
           <i className="icon-picture"></i>Receive
         </label>
-        <div className="tab-content-container">
-          {activeTab === "tab1" ? <SendTab /> : <ReceiveTab />}
-        </div>
-        {/* <ul>
-          <li className="tab-content tab-content-first typography">
-            <textarea></textarea>
-            <button>Send</button>
-            <button>Reset</button>
+
+        <ul>
+          <li className="tab-content-first">
+            <SendTab status={status} setStatus={setStatus} />
           </li>
 
-          <li className="tab-content tab-content-2 typography">
-            <input type="text" placeholder="code" />
-            <button>download</button>
+          <li className="tab-content-2">
+            <ReceiveTab status={status} setStatus={setStatus} />
           </li>
-        </ul> */}
+        </ul>
       </div>
     </div>
   );
--- send-snippet/src/components/tabs/sendTab/sendTab.js
+++ send-snippet/src/components/tabs/sendTab/sendTab.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SendTab.css";
 
-export default function sendTab() {
+export default function SendTab(props) {
+  const [tts, setTts] = useState("");
+  const handleTtsChange = (e) => {
+    setTts(e.target.value);
+  };
   return (
-    <div className="tab-content">
-      <textarea></textarea>
+    <div className="tab-content" id="sendTab-container" autoFocus>
+      <textarea autoFocus value={tts} onChange={handleTtsChange} id="sendTab-textarea"></textarea>
       <button>Send</button>
       <button>Reset</button>
     </div>
