f59b8d455e1f1f5da8c5d30aa6acc6edebbb8859
Nick <nicolaslee2000@gmail.com>
2024-03-12 11:35:32 +0100

    feat(PincodeInput.tsx): fixed indexing

--- src/components/PincodeInput/PincodeInput.tsx
+++ src/components/PincodeInput/PincodeInput.tsx
@@ -6,13 +6,12 @@ export default function PincodeInput(props: any) {
   const digitKey = props.digitKey;
   const setDigitKey = props.setDigitKey;
   // refs for controlling focus on input
-  const pinboxRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
   //for shake animation when entered with wrong/incomplete key
   const [shake, setShake] = useState(false);
   const containerRef = useRef(null);
-  const handleChange = (e: any, i: any) => {
+  const handleChange = (target: any, i: any) => {
     const changedKey = [...digitKey];
-    changedKey[i] = /^\d$/.test(e.target.value) ? e.target.value : "";
+    changedKey[i] = /^\d$/.test(target.value) ? target.value : "";
     setDigitKey(changedKey);
   };
   const handlePaste = (e: any) => {
@@ -26,16 +25,18 @@ export default function PincodeInput(props: any) {
     e.target.select();
   };
   const handleKeyDown = (e: any) => {
-    const index = Number(e.target.attributes.index.value);
-    if (e.key === "ArrowRight" && index < props.DIGIT - 1) {
+    const next = e.target.nextElementSibling;
+    const prev = e.target.previousElementSibling;
+    const id = Number(e.target.id);
+    if (e.key === "ArrowRight" && id !== props.DIGIT - 1) {
       e.preventDefault();
-      pinboxRefs[index + 1].current.focus();
+      next.focus();
     }
-    if (e.key === "ArrowLeft" && index > 0) {
+    if (e.key === "ArrowLeft" && id !== 0) {
       e.preventDefault();
-      pinboxRefs[index - 1].current.focus();
+      prev.focus();
     }
-    if (e.key >= "0" && e.key <= "9" && index < props.DIGIT - 1) {
+    if (e.key >= "0" && e.key <= "9" && id !== props.DIGIT - 1) {
       e.preventDefault();
       if (
         e.target.selectionStart !== e.target.selectionEnd ||
@@ -43,16 +44,13 @@ export default function PincodeInput(props: any) {
       ) {
         //digit selected
         e.target.value = e.key;
-        handleChange(e, index);
-        pinboxRefs[index + 1].current.focus();
+        handleChange(e.target, id);
+        next.focus();
       } else {
         //digit not selected
-        pinboxRefs[index + 1].current.focus();
-        pinboxRefs[index + 1].current.value = e.key;
-        //manual handleChange
-        const changedKey = [...digitKey];
-        changedKey[index + 1] = /^\d$/.test(e.key) ? e.key : "";
-        setDigitKey(changedKey);
+        next.value = e.key;
+        handleChange(next, id + 1);
+        next.focus();
       }
     }
   };
@@ -75,12 +73,11 @@ export default function PincodeInput(props: any) {
               autoComplete="no"
               autoFocus={i === 0}
               value={digit}
-              onChange={(e) => handleChange(e, i)}
-              ref={pinboxRefs[i]}
+              onChange={(e) => handleChange(e.target, i)}
               onKeyDown={handleKeyDown}
               onFocus={handleOnFocus}
               onPaste={handlePaste}
-              index={i}
+              id={i}
             />
           );
         })}
