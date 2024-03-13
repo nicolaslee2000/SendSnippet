5472732cdbb2960b321d161565b6a72e7109c79a
Nick <nicolaslee2000@gmail.com>
2024-03-13 15:51:18 +0100

    feat: environment variable fixed?

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent, setUserId } from "firebase/analytics";
-import { getFirestore, setDoc, doc, serverTimestamp } from "firebase/firestore";
+// import { getFirestore, setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { document } from "./types/document";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apikey,
   authDomain: process.env.REACT_APP_authDomain,
   databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: "sendsnippet",
+  projectId: process.env.REACT_APP_databaseURL,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
@@ -18,26 +18,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const firestore = getFirestore();
+// const firestore = getFirestore();
 
-export const uploadText = async (text: string) => {
-  const tempKey = "8555";
-  const document = doc(
-    firestore,
-    process.env.REACT_APP_FIRESTORE_DEFAULTCOLLECTION_URL!,
-    tempKey
-  );
-  const data: document = {
-    data: text,
-    data_type: "text",
-    created: serverTimestamp(),
-  };
-  try {
-    await setDoc(document, data);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// export const uploadText = async (text: string) => {
+//   const tempKey = "4321";
+//   const document = doc(
+//     firestore,
+//     process.env.REACT_APP_FIRESTORE_DEFAULTCOLLECTION_URL!,
+//     tempKey
+//   );
+//   const data: document = {
+//     data: text,
+//     data_type: "text",
+//     created: serverTimestamp(),
+//   };
+//   try {
+//     await setDoc(document, data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 export const downloadText = async (key: string) => {};
 
